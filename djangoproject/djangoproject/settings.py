37836ddb1fc552641dc606f8d16a6e0b07b1dfdb
tkurudi <t.kurudi209@gmail.com>
2019-03-01 16:07:08 +1300

    AHH

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -73,13 +73,18 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+ DATABASES = {
+     'default': {
+         'ENGINE':'django.db.backends.mysql',
+         'NAME':
+     }
+ }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
